0f11f77614361ea7a14f0beb56591cbc63ad49d7
percy-g2 <gahlotpercy@gmail.com>
2024-08-16 23:57:19 +0530

    minor fixes.

--- composeApp/src/wasmJsMain/kotlin/App.wasmJs.kt
+++ composeApp/src/wasmJsMain/kotlin/App.wasmJs.kt
@@ -1,7 +1,14 @@
 import io.github.xxfast.kstore.KStore
 import io.github.xxfast.kstore.storage.storeOf
+import model.CryptoPair
 import ui.Settings
 
 actual fun getKStore(): KStore<Settings> {
-    return storeOf<Settings>("settings")
+    return storeOf<Settings>(
+        key = "settings",
+        default = Settings(
+            selectedTheme = 0,
+            favPairs = listOf(CryptoPair.BTCUSDT.symbol, CryptoPair.ETHUSDT.symbol, CryptoPair.SOLUSDT.symbol)
+        )
+    )
 }
