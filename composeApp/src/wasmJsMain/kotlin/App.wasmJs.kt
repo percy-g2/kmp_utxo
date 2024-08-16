f427a1514f4ad15acb79c554a991cf8cda552035
percy-g2 <percy-g2@users.noreply.github.com>
2024-08-16 17:54:20 +0000

    Deploying to gh-pages from @ percy-g2/kmp_utxo@e00528616a0142825b62b4b1b8b3ed364dadc132 🚀

--- composeApp/src/wasmJsMain/kotlin/App.wasmJs.kt
+++
@@ -1,7 +0,0 @@
-import io.github.xxfast.kstore.KStore
-import io.github.xxfast.kstore.storage.storeOf
-import ui.Settings
-
-actual fun getKStore(): KStore<Settings> {
-    return storeOf<Settings>("settings")
-}
