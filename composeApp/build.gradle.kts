c129dbcc569e7d56ee7c2a9058ead5267b7861c4
percy-g2 <gahlotpercy@gmail.com>
2024-12-05 19:32:18 +0530

    Handle system insets to accommodate device notches and system bars. Fix previews.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,6 +1,5 @@
 
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
-import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
@@ -24,7 +23,6 @@ kotlin {
         binaries.executable()
     }
     androidTarget {
-        @OptIn(ExperimentalKotlinGradlePluginApi::class)
         compilerOptions {
             jvmTarget.set(JvmTarget.JVM_11)
         }
--- composeApp/src/androidMain/kotlin/org/androdevlinux/utxo/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/org/androdevlinux/utxo/MainActivity.kt
@@ -8,19 +8,26 @@ import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.asPaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.systemBars
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
-import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.startup.Initializer
 import kotlinx.datetime.Clock
 import kotlinx.datetime.TimeZone
 import kotlinx.datetime.toLocalDateTime
+import model.UiKline
 import theme.DarkColorScheme
 import theme.LightColorScheme
 import theme.ThemeManager
@@ -60,7 +67,13 @@ class MainActivity : ComponentActivity() {
                     }
                 }
             }
-            App()
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(top = WindowInsets.systemBars.asPaddingValues().calculateTopPadding())
+            ) {
+                App()
+            }
         }
     }
 }
@@ -72,16 +85,98 @@ fun Preview() {
     App()
 }
 
-@Preview(device = Devices.PHONE, showBackground = true, showSystemUi = true)
+@Preview
 @Composable
 fun Preview1() {
     ContextProvider.setContext(LocalContext.current)
-    UTXOTheme(DarkColorScheme) {
+    val sampleTrades = listOf(
+        UiKline(
+            openTime = 1633046400000,
+            openPrice = "42500.00",
+            highPrice = "43000.00",
+            lowPrice = "42000.00",
+            closePrice = "42800.00",
+            volume = "1000.00",
+            closeTime = 1633047300000,
+            quoteAssetVolume = "42800000.00",
+            numberOfTrades = 1000,
+            takerBuyBaseAssetVolume = "500.00",
+            takerBuyQuoteAssetVolume = "21400000.00"
+        ),
+        UiKline(
+            openTime = 1633047300000,
+            openPrice = "42800.00",
+            highPrice = "43200.00",
+            lowPrice = "42600.00",
+            closePrice = "43000.00",
+            volume = "800.00",
+            closeTime = 1633048200000,
+            quoteAssetVolume = "34400000.00",
+            numberOfTrades = 800,
+            takerBuyBaseAssetVolume = "400.00",
+            takerBuyQuoteAssetVolume = "17200000.00"
+        ),
+        UiKline(
+            openTime = 1633048200000,
+            openPrice = "43000.00",
+            highPrice = "43500.00",
+            lowPrice = "42800.00",
+            closePrice = "42900.00",
+            volume = "1200.00",
+            closeTime = 1633049100000,
+            quoteAssetVolume = "51840000.00",
+            numberOfTrades = 1200,
+            takerBuyBaseAssetVolume = "600.00",
+            takerBuyQuoteAssetVolume = "25920000.00"
+        ),
+        UiKline(
+            openTime = 1633048200000,
+            openPrice = "43000.00",
+            highPrice = "43500.00",
+            lowPrice = "42800.00",
+            closePrice = "42950.00",
+            volume = "1200.00",
+            closeTime = 1633049100000,
+            quoteAssetVolume = "51840000.00",
+            numberOfTrades = 1200,
+            takerBuyBaseAssetVolume = "600.00",
+            takerBuyQuoteAssetVolume = "25920000.00"
+        ),
+        UiKline(
+            openTime = 1633048200000,
+            openPrice = "43000.00",
+            highPrice = "43500.00",
+            lowPrice = "42800.00",
+            closePrice = "42960.00",
+            volume = "1200.00",
+            closeTime = 1633049100000,
+            quoteAssetVolume = "51840000.00",
+            numberOfTrades = 1200,
+            takerBuyBaseAssetVolume = "600.00",
+            takerBuyQuoteAssetVolume = "25920000.00"
+        ),
+        UiKline(
+            openTime = 1633048200000,
+            openPrice = "43000.00",
+            highPrice = "43500.00",
+            lowPrice = "42800.00",
+            closePrice = "43000.00",
+            volume = "1200.00",
+            closeTime = 1633049100000,
+            quoteAssetVolume = "51840000.00",
+            numberOfTrades = 1200,
+            takerBuyBaseAssetVolume = "600.00",
+            takerBuyQuoteAssetVolume = "25920000.00"
+        )
+    )
+
+    UTXOTheme(LightColorScheme) {
         TickerCard(
             symbol = "BTCUSDT",
             price = "50000",
             timestamp = Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault()).toString(),
-            trades = emptyList()
+            trades = sampleTrades,
+            priceChangePercent = "-1.60"
         )
     }
 }
