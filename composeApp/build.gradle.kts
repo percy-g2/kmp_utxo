f716890a158b306eee99c5b12400fae9c8e6d142
percy-g2 <gahlotpercy@gmail.com>
2025-10-16 23:20:32 +0530

    enable default browser configuration for wasmJs target to simplify build setup

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -17,21 +17,7 @@ plugins {
 kotlin {
     @OptIn(ExperimentalWasmDsl::class)
     wasmJs {
-        outputModuleName = "composeApp"
-        browser {
-            val rootDirPath = project.rootDir.path
-            val projectDirPath = project.projectDir.path
-            commonWebpackConfig {
-                outputFileName = "composeApp.js"
-                devServer = (devServer ?: KotlinWebpackConfig.DevServer()).apply {
-                    static = (static ?: mutableListOf()).apply {
-                        // Serve sources to debug inside browser
-                        add(rootDirPath)
-                        add(projectDirPath)
-                    }
-                }
-            }
-        }
+        browser()
         binaries.executable()
     }
     androidTarget {
